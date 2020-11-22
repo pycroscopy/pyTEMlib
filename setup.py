9fd53d23f5518e0ef40efadce64abb845b1b5210
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-11-22 08:30:00 -0500

    Added more pypi specific information

--- setup.py
+++ setup.py
@@ -25,9 +25,25 @@ setuptools.setup(
     packages=["pyTEMlib"],
     package_data={"pyTEMlib": ["data/*"]},
     install_requires=['scipy', 'numpy',  'pillow', 'simpleITK','ase','pyNSID'],#,'PyQt5> 1.0'],#
+    tests_require=['pytest'],
+    platforms=['Linux', 'Mac OSX', 'Windows 10/8.1/8/7'],
+    # package_data={'sample':['dataset_1.dat']}
+    test_suite='pytest',
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha"
-    ],
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering :: Information Analysis'],
     include_package_data=True,
     entry_points={
         'console_scripts': [
