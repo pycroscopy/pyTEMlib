c6599ea69922634600138b264a2a446898599d85
Gerd Duscher <50049264+gduscher@users.noreply.github.com>
2022-08-22 18:10:19 -0400

    Update config_dir.py

--- pyTEMlib/config_dir.py
+++ pyTEMlib/config_dir.py
@@ -9,7 +9,7 @@ config_dir: setup of directory ~/.pyTEMlib for custom sources and database
 import os
 import shutil
 from pkg_resources import resource_filename
-import importlib
+import importlib.resources
 
 config_files = ['microscopes.csv', 'edges_db.csv', 'edges_db.pkl', 'fparam.txt']
 
