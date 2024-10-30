91dc1c9a0a6e34446b8ad89e7ee446da8cd2791b
ahoust17 <88668350+ahoust17@users.noreply.github.com>
2024-10-30 14:38:13 -0400

    fix mistake

--- pyTEMlib/eds_tools.py
+++ pyTEMlib/eds_tools.py
@@ -385,7 +385,7 @@ def get_model(spectrum, start=100):
 def fit_model(spectrum, elements, use_detector_efficiency=False):
     out_tags = get_x_ray_lines(spectrum, elements)
     peaks, pin, keys = initial_model_parameter(spectrum)
-    energy_scale = energy_scale = spectrum.get_spectral_dims(return_axis=True)[0]
+    energy_scale = spectrum.get_spectral_dims(return_axis=True)[0]
 
     if 'detector' in spectrum.metadata['experiment'].keys():
         if 'start_channel' not in spectrum.metadata['experiment']['detector']:
