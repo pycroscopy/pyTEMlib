d550629091bf6c9f8bc0bcba8f5b3de50316ea0c
Gerd Duscher <50049264+gduscher@users.noreply.github.com>
2026-03-06 11:04:45 -0500

    Update eds_tools.py

--- pyTEMlib/eds_tools/eds_tools.py
+++ pyTEMlib/eds_tools/eds_tools.py
@@ -487,8 +487,8 @@ def fit_model(spectrum, use_detector_efficiency=False):
     y = np.array(spectrum)  # .compute()
     [p, _] = scipy.optimize.leastsq(residuals, pin, args=(y,), maxfev=10000)
 
-    update_fit_values(spectrum.metadata['EDS'], peaks, np,abs(p)
-    return np.array(peaks), np.abs(p)
+    update_fit_values(spectrum.metadata['EDS'], peaks, np,abs(p))
+    return np.array(peaks), np.abs(p))
 
 
 def update_fit_values(out_tags, peaks, p):
